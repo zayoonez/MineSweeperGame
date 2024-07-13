4b0c3afcf124077be7a469fb63b6cf782a5e5ac9
JayoonKim <jayoon999@gmail.com>
2024-07-13 17:48:56 +0900

    feat: 초기 세팅 및 라이브러리 설치

---
+++ src/redux/slice/slice.tsx
@@ -0,0 +1,14 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+interface State {}
+const initialState: State = {};
+// const typeSlice = createSlice({
+// //   name: "mi",
+// //   initialState,
+//   reducers: {},
+//   },
+// );
+
+// export const { setSelectedOption } = typeSlice.actions;
+
+// export default typeSlice.reducer;
